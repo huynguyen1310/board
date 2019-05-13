5eb3abf83fa483595c4f9f122060e185f390cc35
Huy <qhuy131096@gmail.com>
2019-05-13 20:38:07 +0700

    ep7

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -9,14 +9,22 @@ class ProjectsController extends Controller
 {
     public function index() 
     {
-        $projects = Project::all();
+        $projects = auth()->user()->projects;
 
         return view('projects.index',compact('projects'));
     }
 
-    public function show(Project $project) {
-        
+    public function show(Project $project) 
+    {
+        if(auth()->user()->isNot($project->owner)){
+            abort(403);
+        }
+
         return view('projects.show' , compact('project'));
+    }
+
+    public function create() {
+        return view('projects.create');
     }
 
 
