5b81615ef573ef69539a74f5b08d9199f7ac0bc0
Huy <qhuy131096@gmail.com>
2019-05-21 20:53:16 +0700

    deloy

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -8,13 +8,13 @@
         </div>
     </header>
 
-    <main class="flex flex-wrap -mx-3">
+    <main class="flex flex-wrap -mx-3 ">
         @forelse ($projects as $project)
             <div class="w-1/3 px-3 pb-6">
                 @include('projects.card')
             </div>
         @empty
-            <p>No record at this moment</p>
+            <p class="text-default">No record at this moment</p>
         @endforelse
     </main>
 
